8b2821c1b25c107d4c905c54199b044fccc80914
Jonas Gottlieb <jonas.gottlieb@stackit.cloud>
2026-01-08 16:51:06 +0100

    fix: remove requiresreplace from system_routes flag
    When changing the boolean flag of system_routes on network-area routing tables it is not needed to recreate the routing table anymore but it can be updated without.

--- stackit/internal/services/iaasalpha/routingtable/table/resource.go
+++ stackit/internal/services/iaasalpha/routingtable/table/resource.go
@@ -8,7 +8,6 @@ import (
 	"time"
 
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/booldefault"
-	"github.com/hashicorp/terraform-plugin-framework/resource/schema/boolplanmodifier"
 
 	iaasUtils "github.com/stackitcloud/terraform-provider-stackit/stackit/internal/services/iaas/utils"
 
@@ -203,9 +202,6 @@ func (r *routingTableResource) Schema(_ context.Context, _ resource.SchemaReques
 				Optional:    true,
 				Computed:    true,
 				Default:     booldefault.StaticBool(true),
-				PlanModifiers: []planmodifier.Bool{
-					boolplanmodifier.RequiresReplace(),
-				},
 			},
 			"dynamic_routes": schema.BoolAttribute{
 				Description: "This controls whether dynamic routes are propagated to this routing table",
@@ -532,5 +528,6 @@ func toUpdatePayload(ctx context.Context, model *Model, currentLabels types.Map)
 		Name:          conversion.StringValueToPointer(model.Name),
 		Labels:        &labels,
 		DynamicRoutes: conversion.BoolValueToPointer(model.DynamicRoutes),
+		SystemRoutes:  conversion.BoolValueToPointer(model.SystemRoutes),
 	}, nil
 }
